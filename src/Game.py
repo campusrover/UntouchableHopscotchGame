00fc096312b5464c14bf6951e599a78329314412
Al Colon <alcolon@brandeis.edu>
2021-04-27 15:02:45 -0400

    more edits

--- src/Game.py
+++ src/Game.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
-
 import rospy
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-from tf.transformations import euler_from_quaternion
+from std_msgs.msg import String
+# responsible for keeping track of player location and penalty
+
+rospy.init_node('gamenode')
+
+timer = 10 #secs
+who_is_black = 0 # user is black
+
+#Publishers
+gamepub = rospy.Publisher('color_mode', String, queue_size=1)
+
+#Subscribers
+subcpu = rospy.Subscriber('/cpu_robot/odom', Odometry, cpu_odom_cb)
+subuser = rospy.Subscriber('user_robot/odom', Odometry, user_odom_cb)
+
+def cpu_odom_cb(msg):
+    global cpu_x
+    global cpu_y
+    cpu_x = msg.data.pose.pose.position.x
+    cpu_y = msg.data.pose.pose.position.y
+
+def user_odom_cb(msg):
+    global usr_x
+    global usr_y
+    usr_x = msg.data.pose.pose.position.x
+    usr_y = msg.data.pose.pose.position.y
+
+while (!rospy.is_shutdown):
--- src/User_Robot.py
+++ src/User_Robot.py
@@ -1,6 +1,51 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
+import sys
+import math
+import tf
+from std_msgs.msg import String
 from nav_msgs.msg import Odometry
-from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+
+
+# it is not necessary to add more code here but it could be useful
+def key_cb(msg):
+   global state; global last_key_press_time
+   state = msg.data
+   last_key_press_time = rospy.Time.now()
+
+# odom is also not necessary but very useful
+def odom_cb(msg):
+   return
+
+# init node
+rospy.init_node('dancer')
+
+# subscribers/publishers
+
+
+# RUN rosrun prrexamples key_publisher.py to get /keys
+key_sub = rospy.Subscriber('keys', String, key_cb)
+odom_sub = rospy.Subscriber('odom', Odometry, odom_cb)
+cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+# start in state halted and grab the current time
+state = "H"
+last_key_press_time = rospy.Time.now()
+
+# set rate
+rate = rospy.Rate(10)
+
+# Wait for published topics, exit on ^c
+while not rospy.is_shutdown():
+
+   # print out the current state and time since last key press
+   print_state()
+
+
+
+   # run at 10hz
+   rate.sleep()
